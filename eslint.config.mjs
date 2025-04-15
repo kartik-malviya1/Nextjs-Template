f0b31df74fb825d98a5c2a628d78ffafffd27cbb
kartik-malviya1 <kartikmalviya30@gmail.com>
2025-04-15 19:23:13 +0530

    refactor: update configuration files and remove unused components
    - Updated .prettierrc.json to enhance import ordering and added plugins.
    
    - Modified .vscode/settings.json to include file associations and TypeScript SDK path.
    
    - Enhanced eslint.config.mjs with new plugins and rules for file naming conventions.
    
    - Adjusted next.config.ts for experimental features.
    
    - Updated package-lock.json and package.json for dependency versions and added new packages.
    
    - Removed example component and favicon.ico.
    
    - Cleaned up globals.css by removing unused CSS variables and themes.
    
    - Revised layout.tsx to simplify structure and update metadata.
    
    - Updated tsconfig.json to use ESNext as the target and module.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,8 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import checkFile from 'eslint-plugin-check-file';
+
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -15,11 +17,19 @@ const eslintConfig = ([
     "next/typescript",
   ),
   {
+    plugins: {
+      "check-file": [checkFile],
+    },
     rules: {
       "prefer-arrow-callback": ["error"],
       "prefer-template": ["error"],
       "semi": ["error"],
-      "quotes": ["error", "double"]
+      "quotes": ["error", "double"],
+      "check-file/filename-naming-convention": ["error",
+        {"**/*.{ts,tsx}": "KEBAB_CASE"},
+        {"ignoreMiddleExtensions": true}],
+      "check-file/folder-naming-convention": ["error",
+        {"src/**/": "KEBAB_CASE"}]
     }
   }
 ]);
