19ecb0ee1826a7ff2a97ba5bcebb7fc8ca15339f
kartik-malviya1 <kartikmalviya30@gmail.com>
2025-04-15 17:35:15 +0530

    initial commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,8 +9,19 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+const eslintConfig = ([
+  ...compat.extends(
+    "next/core-web-vitals",
+    "next/typescript",
+  ),
+  {
+    rules: {
+      "prefer-arrow-callback": ["error"],
+      "prefer-template": ["error"],
+      "semi": ["error"],
+      "quotes": ["error", "double"]
+    }
+  }
+]);
 
 export default eslintConfig;
